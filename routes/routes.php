8970bab0bd36dc931a3025193cc124a3f77e2f06
kaiyeej <58097285+kaiyeej@users.noreply.github.com>
2023-02-25 09:56:45 +0800

    online

--- routes/routes.php
+++ routes/routes.php
@@ -34,7 +34,7 @@ $routes = array(
 
 
 $base_folder = "pages/";
-$page = str_replace("/medplant/", "", $request);
+$page = str_replace("/medplants/", "", $request);
 
 // chec if has parameters
 if (substr_count($page, "?") > 0) {
